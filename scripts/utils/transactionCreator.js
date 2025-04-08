c1c056f6882b0c976ec7b4e716302936b4977f23
octopus <912554887@qq.com>
2025-04-08 10:36:15 +0800

    update maxFeePerGas

--- scripts/utils/transactionCreator.js
+++ scripts/utils/transactionCreator.js
@@ -292,7 +292,7 @@ function createLegacyTransactionWithSig(chainId, nonce, feeData, gasLimit, from,
  */
 function createEip1559Transaction(chainId, nonce, feeData, gasLimit, from, to, value, data, accessList, wallet) {
 
-    const maxFeePerGas = /*feeData.maxFeePerGas ||*/ ethers.parseUnits("0.5", "gwei");
+    const maxFeePerGas = /*feeData.maxFeePerGas ||*/ ethers.parseUnits("0.8", "gwei");
     const maxPriorityFeePerGas = /*feeData.maxPriorityFeePerGas ||*/ ethers.parseUnits("0.00000001", "gwei");
 
     // EIP1559 交易的字段顺序: 0x02 || rlp([chain_id, nonce, max_priority_fee_per_gas, max_fee_per_gas, gas_limit, destination, amount, data, access_list, signature_y_parity, signature_r, signature_s]) 
@@ -358,9 +358,7 @@ function createEip1559TransactionWithSig(chainId, nonce, feeData, gasLimit, from
 
     // const gasPrice = feeData.gasPrice || ethers.parseUnits("30", "gwei");
 
-    // console.log(" ### ===> feeData", feeData);
-
-    const maxFeePerGas = /*feeData.maxFeePerGas ||*/ ethers.parseUnits("0.5", "gwei");
+    const maxFeePerGas = /*feeData.maxFeePerGas ||*/ ethers.parseUnits("0.8", "gwei");
     const maxPriorityFeePerGas = /*feeData.maxPriorityFeePerGas ||*/ ethers.parseUnits("0.00000001", "gwei");
 
     // EIP1559 交易的字段顺序: 0x02 || rlp([chain_id, nonce, max_priority_fee_per_gas, max_fee_per_gas, gas_limit, destination, amount, data, access_list, signature_y_parity, signature_r, signature_s]) 
@@ -462,7 +460,7 @@ function createEip2930Transaction(chainId, nonce, feeData, gasLimit, from, to, v
 
     const gasPrice = feeData.gasPrice || ethers.parseUnits("30", "gwei");
 
-    const maxFeePerGas = /*feeData.maxFeePerGas ||*/ ethers.parseUnits("0.0000003", "gwei");
+    const maxFeePerGas = /*feeData.maxFeePerGas ||*/ ethers.parseUnits("0.8", "gwei");
     const maxPriorityFeePerGas = /*feeData.maxPriorityFeePerGas ||*/ ethers.parseUnits("0.00000001", "gwei");
 
     // 0x01 || rlp([chainId, nonce, gasPrice, gasLimit, to, value, data, accessList, signatureYParity, signatureR, signatureS])
